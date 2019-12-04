c8ca44acce50c3538e85613f33c51b05000dbf91
Dylan McCall <dylan@endlessm.com>
2019-12-04 13:42:29 -0800

    Add Python setuptools script

--- kolibri_webview_demo/__main__.py
+++ kolibri_webview_demo/__main__.py
@@ -2,6 +2,9 @@ import sys
 
 from .application import Application
 
-if __name__ == "__main__":
+def main():
     app = Application()
     sys.exit(app.run(sys.argv))
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kolibri-webview-demo",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'kolibri_webview_demo': ['template/*']
+    },
+    entry_points={
+        'console_scripts': [
+            'kolibri_webview_demo = kolibri_webview_demo.__main__:main'
+        ]
+    },
+    python_requires='>=3.6',
+    install_requires=[
+        'PyGObject>=3.34.0',
+        'SQLAlchemy>=1.3.11',
+    ]
+)
